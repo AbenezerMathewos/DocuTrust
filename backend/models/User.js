cff5b9cb33c72a02e78b423fd9df1558adce6a52
Abenezer <mathewosa9@gmail.com>
2026-08-19 20:30:31 -0600

    feat: update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -25,6 +25,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     enum: ['user', 'admin'],
     default: 'user',
+    lowercase: true,   // auto-lowercase on save
   },
 }, { timestamps: true });
 
