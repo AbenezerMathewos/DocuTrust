78c2a37bacab66c5fbc7a21f9678f69f9564cf63
Abenezer <mathewosa9@gmail.com>
2026-08-19 20:34:49 -0600

    feat: update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const helmet = require('helmet');
 const hpp = require('hpp');
 const rateLimit = require('express-rate-limit');
 
