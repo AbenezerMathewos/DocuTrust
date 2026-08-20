d1296564fcb2f38af659477fe121343122205ea8
Abenezer <mathewosa9@gmail.com>
2026-08-19 20:57:26 -0600

    feat: update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,10 +3,6 @@ const { register, login } = require('../controllers/authController');
 
 const router = express.Router();
 
-router.use((req, res, next) => {
-  console.log(`[AUTH ROUTE] ${req.method} ${req.path} body:`, JSON.stringify(req.body));
-  next();
-});
 
 router.post('/register', register);
 router.post('/login', login);
