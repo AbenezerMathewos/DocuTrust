ce00aa1f243817afb1a69089c537dcbd27914c20
Abenezer <mathewosa9@gmail.com>
2026-08-21 18:31:17 -0600

    feat: update auditRoutes.js

--- backend/routes/auditRoutes.js
+++ backend/routes/auditRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getAuditLogs } = require('../controllers/auditController');
+const { getAuditLogs, getAdminStats, getBlockchainLedger } = require('../controllers/auditController');
 const { protect, authorize } = require('../middleware/authMiddleware');
 
 router.get('/', protect, authorize('root_admin', 'issuer'), getAuditLogs);
+router.get('/stats', protect, authorize('root_admin'), getAdminStats);
+router.get('/blockchain', protect, authorize('root_admin'), getBlockchainLedger);
 
 module.exports = router;
