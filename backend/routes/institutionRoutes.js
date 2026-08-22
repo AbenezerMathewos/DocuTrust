5c8797191402adc37258dba18b83ea67a6800630
Abenezer <mathewosa9@gmail.com>
2026-08-21 19:08:51 -0600

    feat: update institutionRoutes.js

--- backend/routes/institutionRoutes.js
+++ backend/routes/institutionRoutes.js
@@ -7,16 +7,17 @@ const {
   updateInstitution,
   toggleInstitutionStatus,
 } = require('../controllers/institutionController');
+const { protect, authorize } = require('../middleware/authMiddleware');
 
 router.route('/')
-  .get(getInstitutions)
-  .post(createInstitution);
+  .get(protect, authorize('root_admin', 'issuer'), getInstitutions)
+  .post(protect, authorize('root_admin'), createInstitution);
 
 router.route('/:id')
-  .get(getInstitutionById)
-  .put(updateInstitution);
+  .get(protect, authorize('root_admin'), getInstitutionById)
+  .put(protect, authorize('root_admin'), updateInstitution);
 
 router.route('/:id/status')
-  .patch(toggleInstitutionStatus);
+  .patch(protect, authorize('root_admin'), toggleInstitutionStatus);
 
 module.exports = router;
