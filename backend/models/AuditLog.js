70ff4403b629e8486ffd8e7d395732ab03820df6
Abenezer <mathewosa9@gmail.com>
2026-08-21 19:24:09 -0600

    feat: update AuditLog.js

--- backend/models/AuditLog.js
+++ backend/models/AuditLog.js
@@ -4,7 +4,7 @@ const auditLogSchema = new mongoose.Schema({
   action: {
     type: String,
     required: true,
-    enum: ['ISSUE_SINGLE', 'ISSUE_BATCH', 'REVOKE', 'SYSTEM_INIT']
+    enum: ['ISSUE_SINGLE', 'ISSUE_BATCH', 'REVOKE', 'SYSTEM_INIT', 'VERIFY', 'LOGIN', 'INSTITUTION_TOGGLE', 'DOWNLOAD']
   },
   actor: {
     type: String,
