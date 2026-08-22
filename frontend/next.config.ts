67aee06664c4edd8bc5e6bfd5cee356739981240
Abenezer <mathewosa9@gmail.com>
2026-08-21 21:24:12 -0600

    feat: update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['thumbzilla-analysts-library-clicks.trycloudflare.com'],
+  devIndicators: {
+    appIsrStatus: false,
+    buildActivity: false,
+    buildActivityPosition: 'bottom-right',
+  }
 };
 
 export default nextConfig;
